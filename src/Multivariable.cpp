0b894d46b5fa4e9b7f2674cc2b76663e3dc4c15e
Mildred34 <alexis-h-34@hotmail.fr>
2018-10-17 01:46:43 +0200

    Fonction somme avec nombre de paramètres non fixe

--- src/Multivariable.cpp
+++ src/Multivariable.cpp
@@ -7,9 +7,29 @@
 //============================================================================
 
 #include <iostream>
+#include <stdarg.h>
 using namespace std;
 
+
+int sum(int n, ...)
+{
+   int somme = 0;
+   va_list va;
+   va_start (va, n);
+   int i;
+
+   for (i = 0; i < n; i++)
+      {
+         somme += va_arg (va, int);
+      }
+
+   va_end (va);
+   return somme;
+}
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	int somme;
+	somme = sum(2,2,4);
+	cout << somme << endl;
 	return 0;
 }
